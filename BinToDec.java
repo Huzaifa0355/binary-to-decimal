c868dc797f5d0077c0c13c0461d9ef6fceb08234
Huzaifa0355 <huzaifa0359404@gmail.com>
2026-01-06 09:25:05 +0530

    Add files via upload

---
+++ BinToDec.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class BinToDec{
+	public static void binaryToDecimal(int binary){
+		// Scanner sc = new Scanner(System.in);
+		// int binary = sc.nextInt();
+		int binarynum = binary;
+		int pow = 0;
+		int dec = 0;
+		
+		while( binary > 0 ){
+			int lastdigit = binary % 10;
+			dec = dec + (lastdigit * (int)Math.pow(2, pow));
+			
+			pow++;
+			binary /= 10;
+		}
+		System.out.println("binary " + binarynum + " to decimal is : " + dec );
+	}
+	
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("enter binary Number : ");
+		int binary = sc.nextInt();
+		binaryToDecimal(binary);
+	}
+}
